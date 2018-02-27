812826766540a63649f55a64a155e7c1d18651ef
chaifangbo <chaifangbo@zuoyebang.com>
2018-02-27 13:28:58 +0800

    前端上传逻辑更新

--- src/views/admin/post.vue
+++ src/views/admin/post.vue
@@ -2,17 +2,19 @@
     <div class="post-body">
         <el-form>
             <el-form-item label="文章标题">
-                <el-input v-model="form.title"></el-input>
+                <el-input v-model="title"></el-input>
             </el-form-item>
         </el-form>
         <el-upload
             ref="upload"
+            :multiple="false"
             action="/api/admin/posts"
             :on-change="onChange"
             :on-success="onSuccess"
             :on-error="onError"
+            :on-remove="onRemove"
             :auto-upload="false"
-            :data="form">
+            :data="{title: title}">
             <el-button slot="trigger" size="small" type="primary">选择文章</el-button>
             <el-button style="margin-left: 10px;" size="small" type="success" @click="upload">上传至服务器</el-button>
         </el-upload>
@@ -25,24 +27,43 @@
     module.exports = {
         data () {
             return {
-                form: {
-                    title: ''
-                },
+                // form: {},
                 hasSuccess: false,
+                hasError: false,
+                file: null,
             };
+        },
+        computed: {
+            title: {
+                get: function () {
+                    return (this.file && this.file.name) || '';
+                },
+                set: function (val) {
+                    this.file.name = val;
+                }
+            }
         },
         methods: {
             onChange (file, fileList) {
-
+                if (this.hasSuccess || this.hasError) {
+                    if (this.hasSuccess) this.hasSuccess = false;
+                    if (this.hasError) this.hasError = false;
+                    return;
+                }
+                // 本地选择
+                this.file = fileList[0];
             },
             onSuccess () {
-
+                this.hasSuccess = true;
             },
             onError () {
-
+                this.hasError = true;
+            },
+            onRemove (file, fileList) {
+                this.file = fileList[0];
             },
             upload () {
-
+                this.$refs.upload.submit();
             }
         },
         components: {
